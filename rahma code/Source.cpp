4e9cd56dc21899de071cade5b2428df3e6efecd7
rah2009 <nour2009892@gmail.com>
2022-07-28 13:25:11 +0200

    first commit

---
+++ rahma code/Source.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "please enter your number:" << endl;
+	int x;
+	int y;
+	cin >> x;
+	cin >> y;
+	if (x > y)
+	{
+		cout << x << endl;
+	}
+	else
+	{
+		cout << y << endl;
+	}
+
+}
